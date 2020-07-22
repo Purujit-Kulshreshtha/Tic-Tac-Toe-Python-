3335236dfd415a5b7c2b202e9abe175e2960679b
Purujit-Kulshreshtha <purujitkulshreshtha8@gmail.com>
2020-07-22 23:35:45 +0530

    Added Naughts from grid

--- AppClass.py
+++ AppClass.py
@@ -10,6 +10,8 @@ class App():
 		self.state = "playing"
 		self.turn = True
 		self.block = [0, 0]
+		self.clicked = None
+		self.board = BOARD
 
 	def run(self):
 		while self.running:
@@ -26,16 +28,16 @@ class App():
 
 			#on click
 			if event.type == pygame.MOUSEBUTTONDOWN:
-				if self.turn == True:
-					self.draw_cross(self.block)
-				elif self.turn == False:
-					self.draw_naught(self.block)
-
+				self.clicked = True
+			else:
+				self.clicked = None
 
 	def playing_draw(self):
 		self.window.fill(BG)
 
 		self.draw_board(self.window)
+
+		self.draw_symbols(self.window)
 
 		pygame.display.update()
 
@@ -49,11 +51,20 @@ class App():
 			pygame.draw.line(window, BLACK, (GRID_TRANS[0] + x*(CELL_SIZE), GRID_TRANS[1]), (GRID_TRANS[0] + x*(CELL_SIZE), GRID_TRANS[1] + 450), 3) #vertical line
 			pygame.draw.line(window, BLACK, (GRID_TRANS[0], GRID_TRANS[1] + x*(CELL_SIZE)), (GRID_TRANS[0] + 450, GRID_TRANS[1] + x*(CELL_SIZE)), 3) #horizontalline
 
-	def draw_cross(self, block):
-		print("Drew Cross")
-		self.turn = not self.turn
+	def draw_symbols(self, window):
+		for yindex, row in enumerate(self.board):
+			for xindex, symbol in enumerate(row):
+				if symbol == "0":
+					radius = 50
+					centre_x = 150 * (xindex + 1)
+					centre_y = 175 ^ (yindex + 1)
+					centre = (centre_x, centre_y)
+					pygame.draw.circle(window, BLACK, centre, radius, 3)
 
-	def draw_naught(self, block):
-		print("Drew Naught")
-		self.turn = not self.turn
+				elif symbol == "X":
+					#draw Cross
+					pass
+				else:
+					#draw nothing
+					pass
 
--- Settings.py
+++ Settings.py
@@ -1,6 +1,7 @@
 #colors
 BG = (255, 255, 210)
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
 
 #dimensions
 WIDTH = 600
@@ -8,3 +9,8 @@ HEIGHT = 600
 CELL_SIZE = 150
 GRID_POS = (75, 100)
 GRID_TRANS = (GRID_POS[0], GRID_POS[1], WIDTH - 150, HEIGHT - 150)
+
+#Game board
+BOARD = [["0", "0", "1"],
+		 ["1", "1", "1"],
+		 ["1", "1", "1"]]
